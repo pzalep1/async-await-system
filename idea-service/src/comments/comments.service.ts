271574035da9bf893cc166ecf01ecf97bf8ae8f6
pzalep1 <pz0342@gmail.com>
2020-11-17 19:34:53 -0500

    Finish comments and votes

--- idea-service/src/comments/comments.service.ts
+++ idea-service/src/comments/comments.service.ts
@@ -1,20 +1,97 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Idea } from 'src/entities/idea.entity';
+import { Member } from 'src/entities/member.entity';
+import { Project } from 'src/entities/project.entity';
+import { User } from 'src/entities/user.entity';
+import { Repository } from 'typeorm';
+import { Comment } from 'src/entities/comment.entity';
 
 
 @Injectable()
 export class CommentService {
-  constructor(){}
+  constructor(
+    @InjectRepository(Project) private projectRepository: Repository<Project>,
+    @InjectRepository(Member) private memberRepository: Repository<Member>,
+    @InjectRepository(Idea) private ideaRepository: Repository<Idea>,
+    @InjectRepository(User) private userRepository: Repository<User>,
+    @InjectRepository(Comment) private commentRepository: Repository<Comment>,
+  ){}
 
-  createComment(userId: string, projectId: string, ideaId: string, comment: string): any {
-    
+  async createComment(userId: number, projectId: number, ideaId: number, comment: string, requester: any): Promise<any> {
+    const user = await this.userRepository.findOne({userId});
+    if (user) {
+      const project = await this.projectRepository.findOne({projectId});
+      const userAuthorized = await this.memberRepository.findOne({userId: requester.userId, projectId});
+      if (project && userAuthorized) {
+        const foundIdea = await this.ideaRepository.findOne({ideaId});
+        if (foundIdea) {
+          return this.commentRepository.insert({userId, ideaId, comment});
+        } else {
+          throw new HttpException('Idea not found', HttpStatus.NOT_FOUND);
+        }
+      } else {
+        throw new HttpException('Project not found', HttpStatus.NOT_FOUND);
+      }
+    } else {
+      throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+    }
   }
-  deleteComment(userId: string, projectId: string, ideaId: string): any {
-   
+  async deleteComment(userId: number, projectId: number, ideaId: number, commentId: number, requester: any): Promise<any> {
+    const user = await this.userRepository.findOne({userId});
+    if (user) {
+      const project = await this.projectRepository.findOne({projectId});
+      const userAuthorized = await this.memberRepository.findOne({userId: requester.userId, projectId});
+      if (project && userAuthorized) {
+        const foundIdea = await this.ideaRepository.findOne({ideaId});
+        if (foundIdea) {
+          return this.commentRepository.delete({commentId});
+        } else {
+          throw new HttpException('Idea not found', HttpStatus.NOT_FOUND);
+        }
+      } else {
+        throw new HttpException('Project not found', HttpStatus.NOT_FOUND);
+      }
+    } else {
+      throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+    }
   }
-  updateComment(userId: string, projectId: string, ideaId: string, commentId: string, comment: string): any {
-    
+  async updateComment(userId: number, projectId: number, ideaId: number, commentId: string, comment: string, requester: any): Promise<any> {
+    const user = await this.userRepository.findOne({userId});
+    if (user) {
+      const project = await this.projectRepository.findOne({projectId});
+      const userAuthorized = await this.memberRepository.findOne({userId: requester.userId, projectId});
+      if (project && userAuthorized) {
+        const foundIdea = await this.ideaRepository.findOne({ideaId});
+        if (foundIdea) {
+          return this.commentRepository.update(commentId, { comment });
+        } else {
+          throw new HttpException('Idea not found', HttpStatus.NOT_FOUND);
+        }
+      } else {
+        throw new HttpException('Project not found', HttpStatus.NOT_FOUND);
+      }
+    } else {
+      throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+    }
   }
-  getComment(userId: string, projectId: string, ideaId: string, comment: string): any {
-    
+  async getComment(userId: number, projectId: number, ideaId: number, commentId: number, requester: any): Promise<any> {
+    const user = await this.userRepository.findOne({userId});
+    if (user) {
+      const project = await this.projectRepository.findOne({projectId});
+      const userAuthorized = await this.memberRepository.findOne({userId: requester.userId, projectId});
+      if (project && userAuthorized) {
+        const foundIdea = await this.ideaRepository.findOne({ideaId});
+        if (foundIdea) {
+          return this.commentRepository.find({ commentId });
+        } else {
+          throw new HttpException('Idea not found', HttpStatus.NOT_FOUND);
+        }
+      } else {
+        throw new HttpException('Project not found', HttpStatus.NOT_FOUND);
+      }
+    } else {
+      throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+    }
   }
 }
--- idea-service/src/votes/votes.service.ts
+++ idea-service/src/votes/votes.service.ts
@@ -1,20 +1,97 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Idea } from 'src/entities/idea.entity';
+import { Member } from 'src/entities/member.entity';
+import { Project } from 'src/entities/project.entity';
+import { User } from 'src/entities/user.entity';
+import { Vote } from 'src/entities/vote.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class VotesService {
-  constructor() {
+  constructor(
+    @InjectRepository(Project) private projectRepository: Repository<Project>,
+    @InjectRepository(Member) private memberRepository: Repository<Member>,
+    @InjectRepository(Idea) private ideaRepository: Repository<Idea>,
+    @InjectRepository(User) private userRepository: Repository<User>,
+    @InjectRepository(Vote) private voteRepository: Repository<Vote>,
+  ) {
 
   }
-  getVotesForIdea(projectId: string, ideaId: string): any {
-    
+  async getVotesForIdea(userId: number, projectId: number, ideaId: number, requester: any): Promise<any> {
+    const user = await this.userRepository.findOne({userId});
+    if (user) {
+      const project = await this.projectRepository.findOne({projectId});
+      const userAuthorized = await this.memberRepository.findOne({userId: requester.userId, projectId});
+      if (project && userAuthorized) {
+        const foundIdea = await this.ideaRepository.findOne({ideaId});
+        if (foundIdea) {
+          return this.voteRepository.find({ideaId});
+        } else {
+          throw new HttpException('Idea not found', HttpStatus.NOT_FOUND);
+        }
+      } else {
+        throw new HttpException('Project not found', HttpStatus.NOT_FOUND);
+      }
+    } else {
+      throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+    }
   }
-  addVote(userId: string, vote: boolean): any {
-    
+  async addVote(userId: number, projectId: number, ideaId: number, vote: boolean, requester: any): Promise<any> {
+    const user = await this.userRepository.findOne({userId});
+    if (user) {
+      const project = await this.projectRepository.findOne({projectId});
+      const userAuthorized = await this.memberRepository.findOne({userId: requester.userId, projectId});
+      if (project && userAuthorized) {
+        const foundIdea = await this.ideaRepository.findOne({ideaId});
+        if (foundIdea) {
+          return this.voteRepository.insert({userId, ideaId, vote});
+        } else {
+          throw new HttpException('Idea not found', HttpStatus.NOT_FOUND);
+        }
+      } else {
+        throw new HttpException('Project not found', HttpStatus.NOT_FOUND);
+      }
+    } else {
+      throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+    }
   }
-  deleteVote(userId: string, projectId: string, ideaId: string, voteId: string): any {
-    
+  async deleteVote(userId: number, projectId: number, ideaId: number, voteId: number, requester: any): Promise<any> {
+    const user = await this.userRepository.findOne({userId});
+    if (user) {
+      const project = await this.projectRepository.findOne({projectId});
+      const userAuthorized = await this.memberRepository.findOne({userId: requester.userId, projectId});
+      if (project && userAuthorized) {
+        const foundIdea = await this.ideaRepository.findOne({ideaId});
+        if (foundIdea) {
+          return this.voteRepository.delete({userId, ideaId});
+        } else {
+          throw new HttpException('Idea not found', HttpStatus.NOT_FOUND);
+        }
+      } else {
+        throw new HttpException('Project not found', HttpStatus.NOT_FOUND);
+      }
+    } else {
+      throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+    }
   }
-  updateVote(userId: string, projectId: string, ideaId: string, voteId: string, vote: boolean): any {
-    
+  async updateVote(userId: number, projectId: number, ideaId: number, voteId: number, vote: boolean, requester: any): Promise<any> {
+    const user = await this.userRepository.findOne({userId});
+    if (user) {
+      const project = await this.projectRepository.findOne({projectId});
+      const userAuthorized = await this.memberRepository.findOne({userId: requester.userId, projectId});
+      if (project && userAuthorized) {
+        const foundIdea = await this.ideaRepository.findOne({ideaId});
+        if (foundIdea) {
+          return this.voteRepository.insert({userId, ideaId, vote});
+        } else {
+          throw new HttpException('Idea not found', HttpStatus.NOT_FOUND);
+        }
+      } else {
+        throw new HttpException('Project not found', HttpStatus.NOT_FOUND);
+      }
+    } else {
+      throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+    }
   }
 }
