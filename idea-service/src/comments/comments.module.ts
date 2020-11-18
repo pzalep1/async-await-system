078b31dd7690cf3a446ce9e313a1ac6b64f636d9
pzalep1 <pz0342@gmail.com>
2020-11-17 19:48:49 -0500

    More module fixing

--- idea-service/src/comments/comments.module.ts
+++ idea-service/src/comments/comments.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Administers } from 'src/entities/administers.entity';
+import { Idea } from 'src/entities/idea.entity';
+import { Member } from 'src/entities/member.entity';
+import { Project } from 'src/entities/project.entity';
+import { User } from 'src/entities/user.entity';
+import { Comment } from 'src/entities/comment.entity';
 import { CommentController } from './comments.controller';
 import { CommentService } from './comments.service';
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forFeature([Project]),
+    TypeOrmModule.forFeature([Administers]),
+    TypeOrmModule.forFeature([Member]),
+    TypeOrmModule.forFeature([Idea]),
+    TypeOrmModule.forFeature([User]),
+    TypeOrmModule.forFeature([Comment])
+  ],
   controllers: [CommentController],
   providers: [CommentService],
 })
