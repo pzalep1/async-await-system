67667a026676029215ed357641cd7ddbea5fd664
pzalep1 <pz0342@gmail.com>
2020-11-28 22:03:11 -0500

    Adding module imports

--- async-await/src/app/pages/board/board.module.ts
+++ async-await/src/app/pages/board/board.module.ts
@@ -1,11 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BoardComponent } from './board.component';
+import { FormsModule } from '@angular/forms';
+import {
+  MatCardModule,
+  MatButtonModule,
+  MatIconModule,
+  MatDialogModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatSelectModule,
+} from '@angular/material';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [BoardComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    RouterModule,
+    MatIconModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatInputModule,
+    MatSelectModule,
   ]
 })
 export class BoardModule { }
